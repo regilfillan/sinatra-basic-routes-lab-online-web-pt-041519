e6ed2d7c76dc3b3821eb72fcb931e8729f77fc10
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-26 00:27:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+ get "/name" do
+  "My name is Ryan."
+ end
+ 
+ get "/hometown" do 
+   "My hometown is none of your business."
+ end 
+ 
+ get "/favorite-song" do 
+  "My favorite song is While My Guitar Gently Weeps"
+ end
+  
 end
